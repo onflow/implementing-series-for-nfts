4f7fa9ab183560b594af69e108f1eb6d2df28876
lealobanov <lea.lobanov@icloud.com>
2023-10-17 21:13:40 +0900

    Debugging dates

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
 export const implementingSeriesForNFTs = {
   slug: recipe,
   title: "Implementing Series for NFTs",
-  createdAt: Date(2022, 9, 14),
+  createdAt: new Date(2022, 9, 14),
   author: "Flow Blockchain",
   playgroundLink:
     "https://play.onflow.org/a7d190b6-e0f1-4acc-b34c-f37b39fbab33?type=tx&id=c252ea40-397c-43b0-acfb-c504a7268175&storage=none",
